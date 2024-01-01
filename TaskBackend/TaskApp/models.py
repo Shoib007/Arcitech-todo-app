702fed5cec9886d9cd3fe6db72900f571ddd8fcb
Shoib007 <skhantanda@gmail.com>
2024-01-01 14:10:46 +0530

    Some changes into user model

--- TaskBackend/TaskApp/models.py
+++ TaskBackend/TaskApp/models.py
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email = email, **kwargs)
         user.set_password(password)
-        user.save()
+        user.save(using = self._db)
         return user
     
     def create_superuser(self, email, password, **kwargs):
